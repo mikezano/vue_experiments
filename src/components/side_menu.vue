352659a09fd572caa0bdb8a6d6b1d7ab92349c9d
mikezano <mikezano@gmail.com>
2017-06-15 21:04:31 -0700

    first commit

---
+++ src/components/side_menu.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="side-menu">
+		Hello
+		<ul>
+			<li>A</li>
+			<li>B</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+.side-menu{
+	width:100px;
+	position:absolute;
+	left:0;
+	top:0;
+	bottom:0;
+	border:1px solid black;
+}
+
+.side-menu ul{
+	list-style-type: none;
+	margin:0;
+	padding:0;
+}
+
+
+.side-menu ul li{
+}
+
+</style>
+
