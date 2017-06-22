9fae37e9b7baadfb295f0f957cb98aa29b2a5466
Michael A Manzano <michael.a.manzano@intel.com>
2017-06-22 10:57:34 -0700

    minor change

--- src/components/saraC.vue
+++ src/components/saraC.vue
@@ -6,7 +6,7 @@
         </div>
         <transition name="flip">
             <slot v-if="!isShowing" ></slot>
-            <img v-else  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo14.jpeg" />
+            <!--<img v-else  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo14.jpeg" />-->
         </transition>
     </div>   
 </template>
