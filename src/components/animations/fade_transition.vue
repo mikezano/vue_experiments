34a279ed16261c5596ff1a16c681e537466cf44d
mikezano <mikezano@gmail.com>
2017-09-24 13:44:20 -0700

    Hitting the next button works

--- src/components/animations/fade_transition.vue
+++ src/components/animations/fade_transition.vue
@@ -25,10 +25,23 @@
 					#start.step(@click="next($event)") Start
 					#click.step(@click="next($event)") Click
 					#one-frame.step(@click="next($event)") 1 Frame later
-					#halfway.step(@click="next($event), classHash") Halfway
+					#halfway.step(@click="next($event)") Halfway
 					#done.step(@click="next($event)") Done
 
-</template>
+			.animation-class-container
+				button &#xab; Prev
+				button(@click="advance()") Next &#xbb;
+				div
+					pre 
+						span.purple-code fade-leave 
+						span { opacity: 1;}
+					pre 
+						span.purple-code fade-leave-active
+						span { transition: opacity 5s ease-in;}
+					pre
+						span.purple-code fade-leave-to
+						span {opacity: 0;}
+	</template>
 
 <script>
 export default {
@@ -44,15 +57,16 @@ export default {
 				"emoji fade-enter fade-enter-active",
 				"emoji fade-enter-active fade-enter-to"
 			],
-			classHash: {
-				"#start" : "emoji",
-				"#click" : "emoji fade-leave fade-leave-active",
-				"#one-frame" : "emoji fade-leave-active fade-leave-to",
-				"#halfway" : "emoji fade-leave-active fade-leave-to",
-				"#done" : ""
-			},
+			classHash: [
+				{ id: "#start", class: "emoji"},
+				{ id: "#click", class: "emoji fade-leave fade-leave-active"},
+				{ id: "#one-frame", class: "emoji fade-leave-active fade-leave-to"},
+				{ id: "#halfway", class: "emoji fade-leave-active fade-leave-to"},
+				{ id: "#done", class: ""}
+			],
 			currentClassState:"",
 			currentClassIndex:0,
+			currentHashItem: null,
 			leftOffset:0
 		}
 	},
@@ -64,26 +78,41 @@ export default {
 			.left;
 
 		this.currentClassState = this.classStates[0];
+		this.currentHashItem = this.classHash.filter((obj) => {
+			return obj.id == "#start";
+		})[0];
 	},
 	methods: {
 		stepClick(event){
 			console.log(event);
 		},
-		next(event, classHash){
-			debugger;
+		advance(){
+
+			let j = this.classHash.findIndex((element) => {
+				return element.id == this.currentHashItem.id;
+			});
+			this.currentHashItem = this.classHash[ ( j + 1 ) % this.classHash.length ];
+			
+			console.log(this.currentHashItem);
+			this.advanceDot(this.currentHashItem.id.replace("#",""));
+		},
+		next(event){
 			let element = event.target;
-			console.log(element);
+			this.advanceDot(element.id);
+		},
+		advanceDot(id){
+			let element = document.getElementById(id);
+			let hashKey = "#" + element.id;
 			let coords = element.getBoundingClientRect();
-			console.log(coords);
 			var a = document.getElementById("current");
 			let coords_a = a.getBoundingClientRect();
-			//a.style.top = coords.top+"px";
 			a.style.left = (coords.left - this.leftOffset )+"px";
-			console.log(a);
-			this.currentClassState = this.classHash["#"+element.id];
-			//this.currentClassState = this.classStates[this.currentClassIndex % 6]
-			//this.currentClassIndex++;
-
+			let i = this.classHash.filter((obj)=>{
+				return obj.id == hashKey;
+			})[0];
+			console.log(i);
+			this.currentClassState = i.class;
+			this.currentHashItem = i;
 		},
 		beforeEnter(){
 			alert('beforeEnter');
@@ -124,67 +153,78 @@ export default {
 
 <style lang="scss">
 
-$vue_green: hsla(153, 50%, 48%, 1);
+	$vue_green: hsla(153, 50%, 48%, 1);
 
-.fade-enter-active, .fade-leave-active{
-	transition: opacity 5s ease-in;
-}
-.fade-enter, .fade-leave-to{
-	opacity:0;
-}
-
-.fade-enter-to, .fade-leave{
-	opacity:1;
-}
-
-.time-line-container{
-	width:80%;
-	position:relative;
-	margin-top:30px;
-
-	#current{
-		position: absolute;
-		z-index:2;
-		width:10px;
-		height: 10px;
-		border-radius: 10px;
-		background-color:red;
-		border:2px solid darken(red, 20%);
-		transition:all 0.5s ease-in-out;
+	.fade-enter-active, .fade-leave-active{
+		transition: opacity 5s ease-in;
+	}
+	.fade-enter, .fade-leave-to{
+		opacity:0;
 	}
 
-	.line{
-		background-color:$vue_green;
-		width:100%;
-		height:4px;
-		position: absolute;
-		top:4px;
-		z-index:0;
+	.fade-enter-to, .fade-leave{
+		opacity:1;
 	}
 
-	.time-line{
-		position:absolute;
-		width:100%;
-		display: flex;
-		align-items:center;
-		justify-content: space-around;
-		
-		.step{
-			&:hover{
-				cursor: pointer;
-				border-bottom:2px solid $vue_green;
-			}
-			&:before{
-				content: "";
-				display:block;
-				width:10px;
-				height: 10px;
-				border-radius:10px;
-				background-color:$vue_green;
-				border:2px solid darken($vue_green, 20%);
+	.time-line-container{
+		width:80%;
+		position:relative;
+		margin-top:30px;
+
+		#current{
+			position: absolute;
+			z-index:2;
+			width:10px;
+			height: 10px;
+			border-radius: 10px;
+			background-color:red;
+			border:2px solid darken(red, 20%);
+			transition:all 0.5s ease-in-out;
+		}
+
+		.line{
+			background-color:$vue_green;
+			width:100%;
+			height:4px;
+			position: absolute;
+			top:4px;
+			z-index:0;
+		}
+
+		.time-line{
+			position:absolute;
+			width:100%;
+			display: flex;
+			align-items:center;
+			justify-content: space-around;
+			
+			.step{
+				border-bottom:2px solid transparent;
+				&:hover{
+					cursor: pointer;
+					border-bottom:2px solid $vue_green;
+				}
+				&:before{
+					content: "";
+					display:block;
+					width:10px;
+					height: 10px;
+					border-radius:10px;
+					background-color:$vue_green;
+					border:2px solid darken($vue_green, 20%);
+				}
 			}
 		}
 	}
-}
 
+	.animation-class-container{
+
+		padding-top:30px;
+		pre{
+			font-size:30px;
+		}
+		.purple-code{
+			color: purple;
+		}
+	}
 </style>
