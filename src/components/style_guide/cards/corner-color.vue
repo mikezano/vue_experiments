539d852e6d7e5d50d2e19e5820b9d8b917f24ebc
mikezano <mikezano@gmail.com>
2017-11-20 21:09:51 -0700

    intelligent sassssss

--- src/components/style_guide/cards/corner-color.vue
+++ src/components/style_guide/cards/corner-color.vue
@@ -1,28 +1,29 @@
 <template lang="pug">
 .container
-	.blue-card
-		.blue-card__header 
-			.blue-card__header__indicator  
-			.blue-card__header__text Default
-		.blue-card__content &lt; Praesent massa libero, consectetur nec purus vitaet &gt;
+	.blue.card
+		.card__header 
+			.card__header__indicator  
+			.card__header__text Default
+		.card__content &lt; Praesent massa libero, consectetur nec purus vitaet &gt;
 
-	.red-card
-		.red-card__header 
-			.red-card__header__indicator  
-			.red-card__header__text Danger
-		.red-card__content &lt; Content &gt;
+	.red.card
+		.card__header 
+			.card__header__indicator  
+			.card__header__text Default
+		.card__content &lt; Praesent massa libero, consectetur nec purus vitaet &gt;
 
-	.orange-card
-		.orange-card__header 
-			.orange-card__header__indicator  
-			.orange-card__header__text Warning
-		.orange-card__content &lt; Content &gt;
+	.green.card
+		.card__header 
+			.card__header__indicator  
+			.card__header__text Default
+		.card__content &lt; Praesent massa libero, consectetur nec purus vitaet &gt;
 
-	.green-card
-		.green-card__header 
-			.green-card__header__indicator  
-			.green-card__header__text Success
-		.green-card__content &lt; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent massa libero, consectetur nec purus vitae, vestibulum porta dolor. Suspendisse euismod faucibus dui ut consectetur.  &gt;
+	.orange.card
+		.card__header 
+			.card__header__indicator  
+			.card__header__text Default
+		.card__content &lt; Praesent massa libero, consectetur nec purus vitaet &gt;
+
 </template>
 
 
@@ -34,37 +35,40 @@
 
 <style lang="scss" scoped>
 @mixin corner-card($color, $width){
-	border:1px solid lighten($color,20%);
-	transition: all .2s ease-out;
-	background-color:white;
-	width: $width;
-	margin:4px;
+	$el: card;
+	&.#{$el}{
+		border:1px solid lighten($color,20%);
+		transition: all .2s ease-out;
+		background-color:white;
+		width: $width;
+		margin:4px;
 
-	&:hover{box-shadow:2px 2px 4px gray;}
-	&__header{
-		&__indicator{
-			position:absolute;
-			margin-left:-1px;
-			margin-top:-1px;
-			border-style: solid;
-			border-width: 30px 30px 0 0;
-			border-color: 
-				$color
-				transparent
-				transparent
-				transparent;
+		&:hover{box-shadow:2px 2px 4px gray;}
+		.#{$el}__header{
+			&__indicator{
+				position:absolute;
+				margin-left:-1px;
+				margin-top:-1px;
+				border-style: solid;
+				border-width: 30px 30px 0 0;
+				border-color: 
+					$color
+					transparent
+					transparent
+					transparent;
+			}
+			&__text{
+				text-align:center;
+				font-weight:bold;
+				line-height:30px;
+			}
 		}
-		&__text{
-			text-align:center;
-			font-weight:bold;
-			line-height:30px;
-		}
+		#{$el}__content{padding:4px;}
 	}
-	&__content{padding:4px;}
 }
 
-.blue-card{@include corner-card(#007bff, 200px);}
-.red-card{@include corner-card(#CC0000, 100px);}
-.orange-card{@include corner-card(#FF8800, 100%);}
-.green-card{@include corner-card(#007E33, 250px);}
+.blue{@include corner-card(#007bff, 200px);}
+.red{@include corner-card(#CC0000, 100px);}
+.green{@include corner-card(#FF8800, 90%);}
+.orange{@include corner-card(#007E33, 250px);}
 </style>
