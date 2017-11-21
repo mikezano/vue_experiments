e8e7c80d1d418e247eb68d9b567886c0d03a284d
Michael A Manzano <michael.a.manzano@intel.com>
2017-11-21 08:43:35 -0700

    rollover button

--- src/components/style_guide/buttons/rollover.vue
+++ src/components/style_guide/buttons/rollover.vue
@@ -1,16 +1,25 @@
 <template lang="pug">
 	.container
-		//button.rollover
-			.rollover__container
-				.rollover__main OK
-				.rollover__sub 
-					icon(name="thumbs-up")
+		.good
+			button.rollover
+				.rollover__container
+					.rollover__main GOOD
+					.rollover__sub 
+						icon(name="thumbs-up")
 
-		button.bad.rollover
-			.rollover__container
-				.rollover__main OK
-				.rollover__sub 
-					icon(name="thumbs-down")
+		.bad
+			button.rollover
+				.rollover__container
+					.rollover__main BAD
+					.rollover__sub 
+						icon(name="thumbs-down")
+
+		.bad
+			button.rollover
+				.rollover__container
+					.rollover__main NO
+					.rollover__sub 
+						icon(name="danger")
 </template>
 
 <script>
@@ -22,36 +31,39 @@
 @import '../../../sass/colors.scss';
 
 @mixin rollover($size, $color){
-	
-	width: 2 * $size;
-	height:$size;
-	overflow:hidden;
-	padding:0;
-	border:1px solid darken($color, 10%);
-	color:white;
 
-	&:hover{
-		.rollover__container{
-			margin-top: -1 * $size;
-			cursor:pointer;
+	margin:4px;
+	.rollover{
+		width: 2 * $size;
+		height:$size;
+		overflow:hidden;
+		padding:0;
+		border:1px solid darken($color, 10%);
+		color:white;
+
+		&:hover{
+			.rollover__container{
+				margin-top: -1 * $size;
+				cursor:pointer;
+			}
 		}
-	}
-	&__container{
-		background-color:$color;
-		transition: all .5s ease-in-out;
-	}
-	&__main{
-		height:$size;
-		line-height:$size;
-		font-size: $size/2;
-	}
-	&__sub{
-		height:$size;
-		line-height:$size;		
-		font-size: $size/3;
+		&__container{
+			background-color:$color;
+			transition: all .5s ease-in-out;
+		}
+		&__main{
+			height:$size;
+			line-height:$size;
+			font-size: $size/2;
+		}
+		&__sub{
+			height:$size;
+			line-height:$size;		
+			font-size: $size/3;
+		}
 	}
 	
 }
-//.rollover{ @include rollover(50px, $vue_green);}
-.rollover{ @include rollover(100px, $vue_green);}
+.good{ @include rollover(50px, blue);}
+.bad{ @include rollover(60px, $vue_green);}
 </style>
--- src/components/vuer/vuer.vue
+++ src/components/vuer/vuer.vue
@@ -117,7 +117,7 @@ input:checked + label {
 
 .ex{
 	border:1px solid $vue_green;
-	background-color:  hsla(0, 0%, 92%, .4);
+	background-color:  hsla(0, 0%, 92%, .5);
 	margin:10px;
 	display:grid;
 	grid-template-columns: 40% 60%;
