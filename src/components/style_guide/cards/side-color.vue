e3128fefdb4662886e12efd439dbe96dbeae6077
Michael A Manzano <michael.a.manzano@intel.com>
2017-11-28 13:17:29 -0700

    cleaning up side card

--- src/components/style_guide/cards/side-color.vue
+++ src/components/style_guide/cards/side-color.vue
@@ -1,7 +1,6 @@
 <template lang="pug">
 .container
-
-	.alphabet.scard
+	.fruits.scard
 		.scard__indicator 
 			.scard__indicator__text Fruits
 		.scard__content 
@@ -14,8 +13,6 @@
 				dd Apple
 				dt 🍌:
 				dd Banana
-
-
 	.colors.scard
 		.scard__indicator 
 			.scard__indicator__text Colors
@@ -29,7 +26,6 @@
 				dd #FF8800
 				dt.blue BLUE:
 				dd #007bff
-
 </template>
 
 
@@ -40,13 +36,7 @@
 </script>
 
 <style lang="scss" scoped>
-
-.red{color:#CC0000;}
-.blue{color:#007bff;}
-.orange{color:#FF8800;}
-.green{color:#007E33;}
-
-@mixin side-card($color, $width, $sizing){
+@mixin side-card($color, $titleSize){
 	$gray: hsla(0,0%,80%, 1);
 	$el: scard;
 	&.#{$el}{
@@ -56,15 +46,15 @@
 		background-color:white;
 		.#{$el}__indicator{
 			background-color: $color;
-			width: $sizing + ($sizing/2px);
+			width: $titleSize + ($titleSize/2px);
 			overflow: hidden;
 			text-overflow:ellipsis;
 			
 			&__text{
 				color:white;
 				font-weight:bold;
-				font-size:$sizing;
-				margin-left:$sizing + ($sizing/8px);
+				font-size:$titleSize;
+				margin-left:$titleSize + ($titleSize/8px);
 				margin-top:10px;
 				transform: rotate(90deg);
 				transform-origin: 0 0;
@@ -72,18 +62,22 @@
 		}
 		.#{$el}__content{
 			width:100%;
-			padding:8px 10px;
-			font-size:14px;
+			padding:8px;
 			text-align:left;
-				 dl{display:flex; flex-wrap:wrap;}
-				 dt{
-					font-weight:bold;
-					width: 30%;
-				}
-				dd{color: black; width:50%; margin:0;}
 		}
 	}
 }
-.alphabet{@include side-card(#007bff, 250px, 16px);}
-.colors{@include side-card(#CC0000, 250px, 24px);}
+.fruits{@include side-card(#007bff, 16px);}
+.colors{@include side-card(#CC0000, 30px);}
+
+.fruits, .colors{
+	.red{color:#CC0000;}
+	.blue{color:#007bff;}
+	.orange{color:#FF8800;}
+	.green{color:#007E33;}
+
+	dl{display:flex; flex-wrap:wrap;}
+	dt{font-weight:bold;width: 40%;}
+	dd{color: black; width:60%; margin:0;}
+}
 </style>
